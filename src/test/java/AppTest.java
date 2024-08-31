4e5f622afccfa805820824b0d4eda3692ad7af73
Mykhailo Orban <mikhail.orban@gmail.com>
2024-08-31 19:36:26 +0300

    Initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test
+    void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
